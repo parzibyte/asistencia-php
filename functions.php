5f28812faf3223d3c4e2d87e6e949d9c19143ea3
Luis Cabrera Benito <parzibyte@gmail.com>
2020-11-21 23:10:47 -0600

    Cambios menores y agregando index que redirecciona

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 function getEmployeesWithAttendanceCount($start, $end)
 {
-    $query = " select employees.name, 
+    $query = "select employees.name, 
 sum(case when status = 'presence' then 1 else 0 end) as presence_count,
 sum(case when status = 'absence' then 1 else 0 end) as absence_count 
  from employee_attendance
  inner join employees on employees.id = employee_attendance.employee_id
  where date >= ? and date <= ?
- group by employee_id; ";
+ group by employee_id;";
     $db = getDatabase();
     $statement = $db->prepare($query);
     $statement->execute([$start, $end]);
