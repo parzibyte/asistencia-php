a65567f553c49045f2c84600f04934d8f0aac3cd
Luis Cabrera Benito <parzibyte@gmail.com>
2020-11-22 17:57:42 -0600

    Cambios menores

--- footer.php
+++ footer.php
@@ -1,9 +1,37 @@
+<?php
+/*
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+____________________________________
+/ Si necesitas ayuda, contáctame en \
+\ https://parzibyte.me               /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+Creado por Parzibyte (https://parzibyte.me).
+------------------------------------------------------------------------------------------------
+Si el código es útil para ti, puedes agradecerme siguiéndome: https://parzibyte.me/blog/sigueme/
+Y compartiendo mi blog con tus amigos
+También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0CkGB6AFUoBg?sub_confirmation=1
+------------------------------------------------------------------------------------------------
+*/ ?>
 </main>
 <footer class="px-2 py-2 fixed-bottom bg-dark">
         <span class="text-muted">Attendance system written by
             <a class="text-white" href="//parzibyte.me/blog">Parzibyte</a>
             &nbsp;|&nbsp;
-            <a target="_blank" class="text-white" href="//parzibyte.me/blog">
+            <a target="_blank" class="text-white" href="https://parzibyte.me/blog/2020/11/22/sistema-registro-asistencia-php-mysql/">
                 View source
             </a>
         </span>
--- nav.php
+++ nav.php
@@ -1,9 +1,36 @@
+<?php
+/*
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+____________________________________
+/ Si necesitas ayuda, contáctame en \
+\ https://parzibyte.me               /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+Creado por Parzibyte (https://parzibyte.me).
+------------------------------------------------------------------------------------------------
+Si el código es útil para ti, puedes agradecerme siguiéndome: https://parzibyte.me/blog/sigueme/
+Y compartiendo mi blog con tus amigos
+También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0CkGB6AFUoBg?sub_confirmation=1
+------------------------------------------------------------------------------------------------
+*/ ?>
 <nav class="navbar navbar-expand-md navbar-dark bg-success fixed-top">
     <a class="navbar-brand" href="https://parzibyte.me/blog">
         <img class="img-fluid" style="max-width: 200px" src="img/parzibyte_logo.png" loading="lazy">
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" id="botonMenu"
-            aria-label="Mostrar u ocultar menú">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" id="botonMenu" aria-label="Mostrar u ocultar menú">
         <span class="navbar-toggler-icon"></span>
     </button>
 
@@ -17,6 +44,9 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="attendance_report.php">Attendance report <i class="fa fa-file-alt"></i></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="//parzibyte.me/blog">Support & help&nbsp;<i class="fa fa-hands-helping"></i></a>
             </li>
         </ul>
     </div>
